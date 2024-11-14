2d9fc33c9f97f796556d588e239b170c08a216ac
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2024-11-14 23:50:45 +0530

    updated 63

--- 63.c
+++ 63.c
@@ -2,37 +2,39 @@
 // MEDIUM
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int uniquePathsWithObstacles(int** obstacleGrid, int obstacleGridSize, int* obstacleGridColSize) {
-    int m = obstacleGridSize;
-    int n = *obstacleGridColSize;
-    int array[m][n];
-    array[0][0] = obstacleGrid[0][0] == 0 ? 1 : 0;
-    for (int i = 1; i < m; i ++) {
-        if (obstacleGrid[i][0] == 0 && array[i - 1][0] == 1) {
-            array[i][0] = 1;
-        } else {
-            array[i][0] = 0;
-        }
-    }
+	int** grid = (int**)malloc(obstacleGridSize * sizeof(int*));
+	for (int i = 0; i < obstacleGridSize; i ++) {
+		grid[i] = (int*)calloc(*obstacleGridColSize, sizeof(int));
+	}
+	
+	for (int i = 0; i < obstacleGridSize; i ++) {
+		if (!obstacleGrid[i][0]) {
+			grid[i][0] = 1;
+		} else {
+			break;
+		}
+	}
 
-    for (int i = 1; i < n; i ++) {
-        if (obstacleGrid[0][i] == 0 && array[0][i - 1] == 1) {
-            array[0][i] = 1;
-        } else {
-            array[0][i] = 0;
-        }
-    }
+	for (int j = 0; j < *obstacleGridColSize; j ++) {
+		if (!obstacleGrid[0][j]) {
+			grid[0][j] = 1;
+		} else {
+			break;
+		}
+	}
 
-    for (int i = 1; i < m; i ++) {
-        for (int j = 1; j < n; j ++) {
-            if (obstacleGrid[i][j] == 0) {
-                array[i][j] = array[i - 1][j] + array[i][j - 1];
-            } else {
-                array[i][j] = 0;
-            }
-        }
-    }
+	for (int i = 1; i < obstacleGridSize; i ++) {
+		for (int j = 1; j < *obstacleGridColSize; j ++) {
+			if (!obstacleGrid[i][j]) {
+				grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+			} else {
+				grid[i][j] = 0;
+			}
+		}
+	}
 
-    return array[m - 1][n - 1];
+	return grid[obstacleGridSize - 1][*obstacleGridColSize - 1];
 }
