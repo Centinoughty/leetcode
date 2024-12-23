6a4fd98380f26231651a93c749dcf3fc0f8d8ba1
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2024-12-23 10:44:23 +0530

    added 1346

---
+++ 1346.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_set<int> table;
+
+        for (const int i : arr) {
+            if (table.count(i * 2) > 0 || (i % 2 == 0 && table.count(i / 2) > 0)) {
+                return true;
+            }
+
+            table.insert(i);
+        }
+
+        return false;
+    }
+};
