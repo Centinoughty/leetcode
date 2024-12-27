0402ff720eef24dcce16f84d054fe49502511272
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2024-12-27 17:39:07 +0530

    added 1936

---
+++ 1936.cpp
@@ -0,0 +1,23 @@
+// Add minimum number of rungs
+// MEDIUM
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int addRungs(vector<int>& rungs, int dist) {
+        int prev = 0;
+        int count = 0;
+
+        for (const int x : rungs) {
+            if (x - prev > dist) {
+                count += ceil(static_cast<double>(x - prev) / dist) - 1;
+            }
+
+            prev = x;
+        }
+
+        return count;
+    }
+};
