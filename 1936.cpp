a4c83b6e9e05ad0055ce36c62160417f260d442f
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2024-12-27 17:51:46 +0530

    formatetd 1936

--- 1936.cpp
+++ 1936.cpp
@@ -6,18 +6,18 @@ using namespace std;
 
 class Solution {
 public:
-    int addRungs(vector<int>& rungs, int dist) {
-        int prev = 0;
-        int count = 0;
+  int addRungs(vector<int>& rungs, int dist) {
+    int prev = 0;
+    int count = 0;
 
-        for (const int x : rungs) {
-            if (x - prev > dist) {
-                count += ceil(static_cast<double>(x - prev) / dist) - 1;
-            }
+    for (const int x : rungs) {
+      if (x - prev > dist) {
+        count += ceil(static_cast<double>(x - prev) / dist) - 1;
+      }
 
-            prev = x;
-        }
-
-        return count;
+      prev = x;
     }
+
+    return count;
+  }
 };
