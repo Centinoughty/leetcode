9506c74838c57cd877606b047c46af97cbe7c7fc
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-03-05 05:49:58 +0530

    added 2579

---
+++ 2579.cpp
@@ -0,0 +1,17 @@
+// Count total number of colored cells
+// MEDIUM
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  long long coloredCells(int n) {
+    long long res = 1;
+    for (int i = 1; i < n; i ++) {
+      res += 4 * i;
+    }
+
+    return res;
+  }
+};
