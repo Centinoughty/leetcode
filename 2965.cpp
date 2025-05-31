60de8cb0a52e3aeb22eeaedf3ed1297a749bd369
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-06-01 00:31:24 +0530

    updated 2965

--- 2965.cpp
+++ 2965.cpp
@@ -7,30 +7,27 @@ using namespace std;
 class Solution {
 public:
   vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-    vector<int> res;
-    long long int sum = 0, requiredSum = 0;
-    long long int squared = 0, requiredSquared = 0;
-    
+    long long n = grid.size();
+    long long sum = 0, reqSum = 0;
+    long long sqr = 0, reqSqr = 0;
+
     for (const vector<int> row : grid) {
       for (const int x : row) {
         sum += x;
-        squared += x * x;
+        sqr += x * x;
       }
     }
 
-    long long int n = grid.size();
     n = n * n;
-    requiredSum = (n * (n + 1)) / 2;
-    requiredSquared = (n * (n + 1) * (2 * n + 1)) / 6;
+    reqSum = (n * (n + 1)) / 2;
+    reqSqr = (n * (n + 1) * (2 * n + 1)) / 6;
 
-    int x = requiredSum - sum;
-    int y = requiredSquared - squared;
+    int x = reqSum - sum;
+    int y = reqSqr - sqr;
 
     int a = (y - x * x) / (2 * x);
-    int b = (x * x + y) / (2 * x);
+    int b = (y + x * x) / (2 * x);
 
-    res.push_back(a);
-    res.push_back(b);
-    return res;
+    return {a, b};
   }
 };
