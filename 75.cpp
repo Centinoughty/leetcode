4f85f7c213f4784c8b7de5d0e964ec0c4d70c891
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-05-31 19:09:14 +0530

    added 75

---
+++ 75.cpp
@@ -0,0 +1,34 @@
+// Sort colors
+// MEDIUM
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  void sortColors(vector<int>& nums) {
+    int zero = 0, one = 0, two = 0;
+    for (const int num : nums) {
+      if (num == 0) {
+        zero ++;
+      } else if (num == 1) {
+        one ++;
+      } else {
+        two ++;
+      }
+    }
+
+    for (int i = 0; i < nums.size(); i ++) {
+      if (zero) {
+        nums[i] = 0;
+        zero --;
+      } else if (one) {
+        nums[i] = 1;
+        one --;
+      } else {
+        nums[i] = 2;
+        two --;
+      }
+    }
+  }
+};
