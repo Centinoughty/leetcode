e1b7894aaaf1c5f2dea8c8f4c929895b881a4ce6
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-06-01 23:51:39 +0530

    added approach 128

--- 128.cpp
+++ 128.cpp
@@ -24,3 +24,28 @@ public:
     return maxLength;
   }
 };
+
+
+// Using sort
+class Solution {
+public:
+  int longestConsecutive(vector<int>& nums) {
+    if (nums.size() == 0) return 0;
+
+    sort(nums.begin(), nums.end());
+    int num = nums[0];
+    int count = 1, maxCount = 1;
+    for (int i = 1; i < nums.size(); i ++) {
+      if (nums[i] == nums[i - 1]) continue;
+      else if (nums[i] == num + count) {
+        count ++;
+        maxCount = max(maxCount, count);
+      } else {
+        num = nums[i];
+        count = 1;
+      }
+    }
+
+    return maxCount;
+  }
+};
