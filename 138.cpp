e470b3a807ea69ac77e0461bc4ba71ea15309ee8
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-06-04 23:54:56 +0530

    added approach 138

--- 138.cpp
+++ 138.cpp
@@ -39,3 +39,47 @@ public:
 private:
   unordered_map<Node*, Node*> map;
 };
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+  Node* copyRandomList(Node* head) {
+    Node *temp = new Node(0);
+    Node *next = temp;
+    Node *curr = head;
+    while (curr) {
+      next->next = new Node(curr->val);
+      map[curr] = next->next;
+      curr = curr->next;
+      next = next->next;
+    }
+
+    next = temp->next;
+    curr = head;
+    while (curr) {
+      next->random = map[curr->random];
+      curr = curr->next;
+      next = next->next;
+    }
+
+    return temp->next;
+  }
+
+private:
+  unordered_map<Node*, Node*> map;
+};
