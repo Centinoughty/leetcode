6a9d12f7b1b3036a976b98dc3ec3fb52dcad39b5
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-06-06 16:39:17 +0530

    added approach 26

--- 26.cpp
+++ 26.cpp
@@ -25,3 +25,22 @@ public:
     return count;
   }
 };
+
+class Solution {
+public:
+  int removeDuplicates(vector<int>& nums) {
+    int i = 0;
+    int j = i + 1;
+    while (j < nums.size()) {
+      if (nums[i] == nums[j]) {
+        j ++;
+      } else {
+        nums[i + 1] = nums[j];
+        i ++;
+        j ++;
+      }
+    }
+
+    return i + 1;
+  }
+};
