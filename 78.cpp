c53ed8e5933cb910e6885c5a195611d8df370765
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-06-10 08:03:58 +0530

    updated approach 78

--- 78.cpp
+++ 78.cpp
@@ -9,20 +9,20 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> array;
-        int n = nums.size();
-        for (int i = 0; i < (1 << n); i ++) {
+        vector<vector<int>> res;
+
+        for (int i = 0; i < (1 << nums.size()); i ++) {
             vector<int> temp;
-            for (int j = 0; j < n; j ++) {
-                if ((1 << j) & i) {
+            for (int j = 0; j < nums.size(); j ++) {
+                if (i & (1 << j)) {
                     temp.push_back(nums[j]);
                 }
             }
 
-            array.push_back(temp);
+            res.push_back(temp);
         }
 
-        return array;
+        return res;
     }
 };
 
