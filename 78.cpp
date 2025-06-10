407e29cbada836fe7f4e9f119ff46f46f63f7e0b
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-06-10 07:40:47 +0530

    added approach 78

--- 78.cpp
+++ 78.cpp
@@ -25,3 +25,29 @@ public:
         return array;
     }
 };
+
+// Recursive approach
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        dfs(nums, 0);
+        return res;
+    }
+
+private:
+    vector<vector<int>> res;
+    vector<int> subset;
+    void dfs(vector<int> nums, int i) {
+        if (i >= nums.size()) {
+            res.push_back(subset);
+            return;
+        }
+
+        subset.push_back(nums[i]);
+        dfs(nums, i + 1);
+
+        subset.pop_back();
+        dfs(nums, i + 1);
+    }
+};
