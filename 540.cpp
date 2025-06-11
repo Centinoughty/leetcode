a2ed7bcc829e118326e21cfe6330fa23083f9006
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-06-11 23:55:53 +0530

    added approach 540

--- 540.cpp
+++ 540.cpp
@@ -31,3 +31,21 @@ public:
         return res;
     }
 };
+
+// Binary search
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int l = 0, r = nums.size() - 2;
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            if (nums[mid] == nums[mid ^ 1]) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return nums[l];
+    }
+};
