20775fa628f9ce1f7a3980a1d1df98f808bc1db9
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-06-13 22:53:48 +0530

    added approach 347

--- 347.cpp
+++ 347.cpp
@@ -31,3 +31,31 @@ public:
     return res;
   }
 };
+
+// Better than nlogn
+// use a max heap
+class Solution {
+public:
+    struct CompareVal {
+        bool operator()(const pair<int, int>& a, const pair<int, int>& b) {
+            return a.second < b.second;
+        }
+    };
+
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> res;
+        unordered_map<int, int> count;
+        for (const int num : nums) {
+            count[num]++;
+        }
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, CompareVal> heap(count.begin(), count.end());
+
+        for (int i = 0; i < k; i ++) {
+            res.push_back(heap.top().first);
+            heap.pop();
+        }
+
+        return res;
+    }
+};
