7afd72867e3084eaab022f74d479d2a7c8934453
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-06-14 22:05:47 +0530

    added 225

---
+++ 225.cpp
@@ -0,0 +1,38 @@
+// Implement stack using queues
+// EASY
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class MyStack {
+public:
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        Q.push(x);
+        for (int i = 0; i < Q.size() - 1; i ++) {
+            int temp = Q.front();
+            Q.pop();
+            Q.push(temp);
+        }
+    }
+    
+    int pop() {
+        int temp = Q.front();
+        Q.pop();
+        return temp;
+    }
+    
+    int top() {
+        return Q.front();
+    }
+    
+    bool empty() {
+        return Q.size() == 0;
+    }
+
+private:
+    queue<int> Q;
+};
