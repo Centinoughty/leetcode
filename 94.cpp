7a3fde25e538d77c9c57ce98a052078e777aa0b3
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-06-22 19:07:38 +0530

    added approach 94

--- 94.cpp
+++ 94.cpp
@@ -31,3 +31,34 @@ private:
         }
     }
 };
+
+// Morris inorder traversal
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        TreeNode *curr = root;
+        while (curr) {
+            if (!(curr->left)) {
+                res.push_back(curr->val);
+                curr = curr->right;
+            } else {
+                TreeNode *prev = curr->left;
+                while (prev->right && prev->right != curr) {
+                    prev = prev->right;
+                }
+
+                if (!(prev->right)) {
+                    prev->right = curr;
+                    curr = curr->left;
+                } else {
+                    prev->right = NULL;
+                    res.push_back(curr->val);
+                    curr = curr->right;
+                }
+            }
+        }
+
+        return res;
+    }
+};
