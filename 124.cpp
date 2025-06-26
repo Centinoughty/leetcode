8571c5ef194f2ad40308d597c85d4544eabd5a23
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-06-27 00:38:39 +0530

    added 124

---
+++ 124.cpp
@@ -0,0 +1,35 @@
+// Binary tree maximum path sum
+// HARD
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        dfs(root);
+        return maxSum;
+    }
+
+private:
+    int maxSum = INT_MIN;
+
+    int dfs(TreeNode* root) {
+        if (!root) return 0;
+
+        int left = max(0, dfs(root->left));
+        int right = max(0, dfs(root->right));
+
+        maxSum = max(maxSum, left + right + root->val);
+        return root->val + max(left, right);
+    }
+};
