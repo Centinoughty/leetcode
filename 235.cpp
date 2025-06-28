41e32ad19681e613b15540d580af7bf185d74f4f
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-06-28 17:33:26 +0530

    added 235

---
+++ 235.cpp
@@ -0,0 +1,34 @@
+// Lowest common ancestor of a binary search tree
+// MEDIUM
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (p->val > q->val) {
+            return lowestCommonAncestor(root, q, p);
+        }
+
+        TreeNode* temp = root;
+        int valP = p->val, valQ = q->val;
+
+        while (temp && !(temp->val >= valP && temp->val <= valQ)) {
+            if (valP > temp->val) {
+                temp = temp->right;
+            } else {
+                temp = temp->left;
+            }
+        }
+
+        return temp;
+    }
+};
