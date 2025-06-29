7504394dfc7906729747495444b1a4c693052102
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-06-29 14:21:10 +0530

    added approach 653

--- 653.cpp
+++ 653.cpp
@@ -43,3 +43,72 @@ private:
         }
     }
 };
+
+// Added approach - using BST iterator
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class BSTIterator {
+    stack<TreeNode*> st;
+    bool reverse = false;
+
+public:
+    BSTIterator(TreeNode* root, bool isReverse) {
+        reverse = isReverse;
+        pushNodes(root);
+    }
+
+    bool hasNext() {
+        return !st.empty();
+    }
+
+    int next() {
+        TreeNode* temp = st.top();
+        st.pop();
+        if (reverse) {
+            pushNodes(temp->left);
+        } else {
+            pushNodes(temp->right);
+        }
+
+        return temp->val;
+    }
+
+    void pushNodes(TreeNode* root) {
+        while (root) {
+            st.push(root);
+            if (reverse) {
+                root = root->right;
+            } else {
+                root = root->left;
+            }
+        }
+    }
+};
+
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        BSTIterator l(root, false);
+        BSTIterator r(root, true);
+
+        int i = l.next(), j = r.next();
+        while (i < j) {
+            int sum = i + j;
+            if (sum < k) {
+                i = l.next();
+            } else if (sum > k) {
+                j = r.next();
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
