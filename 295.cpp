362110d9c8b40111264b23d2e11faf3d9a86b341
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-06-30 22:01:07 +0530

    updated 295

--- 295.cpp
+++ 295.cpp
@@ -10,27 +10,26 @@ public:
     
     void addNum(int num) {
         maxHeap.push(num);
-
         if (!minHeap.empty() && maxHeap.top() > minHeap.top()) {
             minHeap.push(maxHeap.top());
             maxHeap.pop();
         }
 
-        if (maxHeap.size() > minHeap.size() + 1) {
-            minHeap.push(maxHeap.top());
-            maxHeap.pop();
-        } else if (minHeap.size() > maxHeap.size() + 1) {
+        if (minHeap.size() > maxHeap.size() + 1) {
             maxHeap.push(minHeap.top());
             minHeap.pop();
+        } else if (maxHeap.size() > minHeap.size() + 1) {
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
         }
     }
     
     double findMedian() {
         int n1 = maxHeap.size(), n2 = minHeap.size();
-        if ((n1 + n2) % 2) {
-            return n1 > n2 ? maxHeap.top() : minHeap.top();
-        } else {
+        if ((n1 + n2) % 2 == 0) {
             return (double)(maxHeap.top() + minHeap.top()) / 2.0;
+        } else {
+            return n1 > n2 ? maxHeap.top() : minHeap.top();
         }
     }
 
