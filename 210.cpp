3fbdd2621a441e69b461416ce11b4a9562d3fd78
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-07-05 07:26:09 +0530

    added approach 210

--- 210.cpp
+++ 210.cpp
@@ -72,3 +72,55 @@ private:
         st.push(pos);
     }
 };
+
+// Optmised approach
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        visited = vector<bool>(numCourses, false);
+        pathVisited = vector<bool>(numCourses, false);
+        vector<vector<int>> graph(numCourses);
+        for (const auto pre : prerequisites) {
+            graph[pre[1]].push_back(pre[0]);
+        }
+
+        for (int i = 0; i < numCourses; i ++) {
+            if (!visited[i]) {
+                if (isCycle(graph, i)) {
+                    return res;
+                }
+            }
+        }
+
+        while (!st.empty()) {
+            res.push_back(st.top());
+            st.pop();
+        }
+
+        return res;
+    }
+
+private:
+    vector<bool> visited, pathVisited;
+    vector<int> res;
+    stack<int> st;
+
+    bool isCycle(vector<vector<int>>& graph, int pos) {
+        visited[pos] = true;
+        pathVisited[pos] = true;
+
+        for (const int ngb : graph[pos]) {
+            if (!visited[ngb]) {
+                if (isCycle(graph, ngb)) {
+                    return true;
+                }
+            } else if (pathVisited[ngb]) {
+                return true;
+            }
+        }
+
+        pathVisited[pos] = false;
+        st.push(pos);
+        return false;
+    }
+};
