5d5162dfac0af9db1f29ebe12bdd57ce637e9c85
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-07-05 08:37:36 +0530

    added approach 1394

--- 1394.cpp
+++ 1394.cpp
@@ -21,3 +21,24 @@ public:
         return -1;
     }
 };
+
+// Optimised space
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        vector<int> mp(arr.size(), 0);
+        for (const int x : arr) {
+            if (x <= arr.size()) {
+                mp[x - 1] ++;
+            }
+        }
+
+        for (int i = arr.size() - 1; i >= 0; i --) {
+            if (mp[i] == i + 1) {
+                return i + 1;
+            }
+        }
+
+        return -1;
+    }
+};
