a33d690138b416b1b27517abb9564d682f13e5e4
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-08-01 16:44:22 +0530

    added approach 152

--- 152.cpp
+++ 152.cpp
@@ -19,3 +19,23 @@ public:
         return maxProd;
     }
 };
+
+// Optimal approach
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int maxProduct = INT_MIN;
+        int prefix = 1, suffix = 1;
+        for (int i = 0; i < n; i ++) {
+            if (prefix == 0) prefix = 1;
+            if (suffix == 0) suffix = 1;
+
+            prefix *= nums[i];
+            suffix *= nums[n - i - 1];
+            maxProduct = max(maxProduct, max(prefix, suffix));
+        }
+
+        return maxProduct;
+    }
+};
