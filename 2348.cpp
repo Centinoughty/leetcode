44730a30ade3a595de378f782b87725b8e9b5d1b
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-08-19 13:59:00 +0530

    added 2348

---
+++ 2348.cpp
@@ -0,0 +1,27 @@
+// Number of zero-filled subarrays
+// MEDIUM
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        int i = 0;
+        ;long long total = 0;
+        long long curr = 0;
+        while (i < nums.size()) {
+            if (nums[i] != 0) {
+                total += (curr * (curr + 1)) / 2;
+                curr = 0;
+            } else {
+                curr ++;
+            }
+
+            i ++;
+        }
+
+        total += (curr * (curr + 1)) / 2;
+        return total;
+    }
+};
