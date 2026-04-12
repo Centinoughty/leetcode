946d2c3843067c1c85ddb06aa5cbb0152df5232e
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-04-13 01:01:13 +0530

    added 3741

---
+++ 3741.cpp
@@ -0,0 +1,33 @@
+// Minimum distance between three equal elements II
+// MEDIUM
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums) {
+        int res = INT_MAX;
+
+        unordered_map<int, queue<int>> mp;
+        for (int i = 0; i < nums.size(); i ++) {
+            if (mp.count(nums[i])) {
+                if (mp[nums[i]].size() == 3) mp[nums[i]].pop();
+            }
+
+            mp[nums[i]].push(i);
+            if (mp[nums[i]].size() == 3) {
+                int acc = 0;
+                int x = mp[nums[i]].front(); mp[nums[i]].pop();
+                int y = mp[nums[i]].front(); mp[nums[i]].pop();
+                int z = mp[nums[i]].front(); mp[nums[i]].pop();
+
+                mp[nums[i]].push(x); mp[nums[i]].push(y); mp[nums[i]].push(z);
+
+                res = min(res, 2 * (z - x));
+            }
+        }
+
+        return res == INT_MAX ? -1 : res;
+    }
+};
