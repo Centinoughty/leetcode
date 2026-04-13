94872957212f5aae1273ab1c59ef7db341fb2a3f
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-04-13 16:46:40 +0530

    added 1848

---
+++ 1848.cpp
@@ -0,0 +1,19 @@
+// Minimum distance to the target element
+// EASY
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        int res = INT_MAX;
+        for (int i = 0; i < nums.size(); i ++) {
+            if (nums[i] == target) {
+                res = min(res, abs(i - start));
+            }
+        }
+
+        return res;
+    }
+};
