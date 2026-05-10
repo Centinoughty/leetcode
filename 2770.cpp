bdefb5902391fd30996498f3676b9b947458f33c
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-05-10 17:07:55 +0530

    added solution 2770

--- 2770.cpp
+++ 2770.cpp
@@ -32,3 +32,24 @@ private:
         return memo[pos];
     }
 };
+
+// DP Solution
+
+class Solution {
+public:
+    int maximumJumps(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> dp(n, INT_MIN);
+        dp[0] = 0;
+
+        for (int i = 1; i < n; i ++) {
+            for (int j = 0; j < i; j ++) {
+                if (abs(nums[i] - nums[j]) <= target) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return dp[n - 1] < 0 ? -1 : dp[n - 1];
+    }
+};
