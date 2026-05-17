4db44e4d7279d60e823e0fce495f20058c3e1b51
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-05-17 09:41:43 +0530

    added 3931

---
+++ 3931.cpp
@@ -0,0 +1,16 @@
+// Check adjascent digit differences
+// EASY
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isAdjacentDiffAtMostTwo(string s) {
+        for (int i = 1; i < s.length(); i ++) {
+            if (abs(s[i] - s[i - 1]) > 2) return false;
+        }
+
+        return true;
+    }
+};
