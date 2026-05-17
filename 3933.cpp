156e6c4114ab187858b6dc9f743c3d1c170b4878
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-05-17 09:42:41 +0530

    added 3933

---
+++ 3933.cpp
@@ -0,0 +1,21 @@
+// largest local values in a matrix II
+// MEDIUM
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countKthRoots(int l, int r, int k) {
+        int newL = ceil(pow(l, (double)1 / k));
+        int newR = floor(pow(r, (double)1 / k));
+
+        if (pow(newL, k) < l) newL ++;
+        if (pow(newR, k) > r) newR --;
+        if (pow(newR + 1, k) <= r) newR ++;
+
+        cout << newL << " " << newR << endl;
+
+        return max(0, newR - newL + 1);
+    }
+};
