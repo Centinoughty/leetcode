709415d234bb42511cb12019ef1f647b2c13cd76
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-05-26 08:45:52 +0530

    added 1351

---
+++ 1351.cpp
@@ -0,0 +1,29 @@
+// Count negative numbers in a sorted array
+// EASY
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        unordered_set<char> st;
+        int cnt = 0;
+        for (const char& c : word) {
+            if (isLower(c)) {
+                if (!st.count(c) && st.count(c - 'a' + 'A')) cnt ++;
+                st.insert(c);
+            } else {
+                if (!st.count(c) && st.count(c - 'A' + 'a')) cnt ++;
+                st.insert(c);
+            }
+        }
+
+        return cnt;
+    }
+
+private:
+    bool isLower(char c) {
+        return 'a' <= c && c <= 'z';
+    }
+};
