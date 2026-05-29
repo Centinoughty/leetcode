7a4cf17233b25ef6873b824d1d7ed6bfbc80178e
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-05-29 12:11:17 +0530

    added 268

---
+++ 268.cpp
@@ -0,0 +1,18 @@
+// Missing number
+// EASY
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int res = 0;
+        for (int i = 0; i < nums.size(); i ++) {
+            res ^= i;
+            res ^= nums[i];
+        }
+
+        return res ^ nums.size();
+    }
+};
