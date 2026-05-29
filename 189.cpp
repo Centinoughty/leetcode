9691514eb18ba9a27d1416cd2736968263802aec
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-05-29 09:28:30 +0530

    added 189

---
+++ 189.cpp
@@ -0,0 +1,21 @@
+// Rotate array
+// MEDIUM
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minElement(vector<int>& nums) {
+        for (int& num : nums) {
+            int temp = num;
+            num = 0;
+            while (temp) {
+                num += temp % 10;
+                temp /= 10;
+            }
+        }
+
+        return *min_element(nums.begin(), nums.end());
+    }
+};
