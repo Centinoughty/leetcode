e1f2f8737a2863f6fb5aaaf94615380557b3d919
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-05-30 13:28:33 +0530

    updated 560

--- 560.cpp
+++ 560.cpp
@@ -21,3 +21,27 @@ public:
     return count;
   }
 };
+
+// Optimal solution
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int cnt = 0;
+        unordered_map<int, int> mp;
+
+        int prefix = 0;
+        for (int i = 0; i < nums.size(); i ++) {
+            prefix += nums[i];
+            if (prefix == k) {
+                cnt ++;
+            }
+
+            if (mp.find(prefix - k) != mp.end()) cnt += mp[prefix - k];
+
+            mp[prefix] ++;
+        }
+
+        return cnt;
+    }
+};
