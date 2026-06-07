0280cada38743337bf8cda281a2f14ff37c563af
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-06-07 21:07:09 +0530

    added solution 57

--- 57.cpp
+++ 57.cpp
@@ -30,3 +30,46 @@ public:
     return res;
   }
 };
+
+// In memory solution
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        if (intervals.size() == 0) {
+            intervals.push_back(newInterval);
+            return intervals;
+        }
+
+        int l = 0;
+        while (l < intervals.size() && newInterval[0] > intervals[l][0]) {
+            l ++;
+        }
+
+        if (l > 0) {
+            if (intervals[l - 1][1] < newInterval[0]) {
+                intervals.insert(intervals.begin() + l, newInterval);
+            } else {
+                intervals[l - 1][1] = max(intervals[l - 1][1], newInterval[1]);
+            }
+        } else {
+            intervals.insert(intervals.begin(), newInterval);
+            l ++;
+        }
+
+        l = min(0, l - 1);
+        for (int r = l + 1; r < intervals.size(); r ++) {
+            int a = intervals[l][0], b = intervals[l][1];
+            int c = intervals[r][0], d = intervals[r][1];
+            
+            if (b < c) {
+                intervals[++l] = intervals[r];
+            } else {
+                intervals[l][1] = max(intervals[l][1], intervals[r][1]);
+            }
+        }
+
+        intervals.resize(l + 1);
+        return intervals;
+    }
+};
