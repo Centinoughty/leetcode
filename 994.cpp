f4624accc0d244ef4a6540cb1e9f47ac7a48507b
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-06-09 10:02:07 +0530

    added solution 994

--- 994.cpp
+++ 994.cpp
@@ -57,3 +57,62 @@ public:
         return maxTime;
     }
 };
+
+// Solution 2
+
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        
+        queue<pair<int, int>> q;
+        for (int i = 0; i < m; i ++) {
+            for (int j = 0; j < n; j ++) {
+                if (grid[i][j] == 2) {
+                    q.push({i, j});
+                }
+            }
+        }
+
+        int count = 0;
+        while (!q.empty()) {
+            int size = q.size();
+
+            while (size --) {
+                auto [x, y] = q.front(); q.pop();
+
+                if (x > 0 && grid[x - 1][y] == 1) {
+                    grid[x - 1][y] = 2;
+                    q.push({x - 1, y});
+                }
+
+                if (y > 0 && grid[x][y - 1] == 1) {
+                    grid[x][y - 1] = 2;
+                    q.push({x, y - 1});
+                }
+
+                if (x < m - 1 && grid[x + 1][y] == 1) {
+                    grid[x + 1][y] = 2;
+                    q.push({x + 1, y});
+                }
+
+                if (y < n - 1 && grid[x][y + 1] == 1) {
+                    grid[x][y + 1] = 2;
+                    q.push({x, y + 1});
+                }
+            }
+
+            count ++;
+        }
+
+        for (int i = 0; i < m; i ++) {
+            for (int j = 0; j < n; j ++) {
+                if (grid[i][j] == 1) {
+                    return -1;
+                }
+            }
+        }
+
+        return max(0, count - 1);
+    }
+};
