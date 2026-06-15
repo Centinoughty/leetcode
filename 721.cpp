c807800516c5385f3e1b1a096aa637a82be658f7
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-06-15 12:59:33 +0530

    added solution 721

--- 721.cpp
+++ 721.cpp
@@ -95,3 +95,88 @@ private:
         return parent[node] = findPar(parent, parent[node]);
     }
 };
+
+// Simple Disjoint Set
+class DisjointSet {
+private:
+    vector<int> rank, parent;
+
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        for (int i = 0; i < n + 1; i ++) {
+            parent[i] = i;
+        }
+    }
+
+    int findPar(int u) {
+        if (u == parent[u]) {
+            return u;
+        }
+
+        return parent[u] = findPar(parent[u]);
+    }
+
+    void unionByRank(int u, int v) {
+        int pu = findPar(u);
+        int pv = findPar(v);
+
+        if (pu == pv) return;
+
+        if (rank[pu] < rank[pv]) {
+            parent[pu] = pv;
+        } else if (rank[pu] > rank[pv]) {
+            parent[pv] = pu;
+        } else {
+            parent[pv] = pu;
+            rank[pu] ++;
+        }
+
+        return;
+    }
+};
+
+class Solution {
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        unordered_map<string, int> mp;
+        unordered_map<int, string> names;
+
+        DisjointSet ds(accounts.size());
+        
+        int pos = 0;
+        for (const vector<string>& account : accounts) {
+            names[pos] = account[0];
+            for (int i = 1; i < account.size(); i ++) {
+                if (mp.find(account[i]) == mp.end()) {
+                    mp[account[i]] = pos;
+                } else {
+                    ds.unionByRank(mp[account[i]], pos);
+                }
+            }
+
+            pos ++;
+        }
+
+        unordered_map<int, int> newPos;
+        vector<vector<string>> res;
+        for (int i = 0; i < pos; i ++) {
+            if (i == ds.findPar(i)) {
+                newPos[i] = res.size();
+                res.push_back({names[i]});
+            }
+        }
+
+        for (const auto& [x, y] : mp) {
+            int py = ds.findPar(y);
+            res[newPos[py]].push_back(x);
+        }
+
+        for (vector<string>& account : res) {
+            sort(account.begin() + 1, account.end());
+        }
+
+        return res;
+    }
+};
