b201ec0bf514c5c9755dfdabfa95d5fde55391b4
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-06-15 07:51:59 +0530

    added 2095

---
+++ 2095.cpp
@@ -0,0 +1,31 @@
+// Delete the Middle Node of a Linked List
+// MEDIUM
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        ListNode *slow = head, *fast = head, *prev = nullptr;
+        while (fast && fast->next) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        if (prev && prev->next) {
+            prev->next = prev->next->next;
+        }
+
+        return prev ? head : prev;
+    }
+};
