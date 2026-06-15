b2e31c525d9e6ab2d18ad5cf46265ee18c10ea4f
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-06-15 11:56:58 +0530

    added solution 947

--- 947.cpp
+++ 947.cpp
@@ -50,3 +50,76 @@ private:
         return parent[node] = findPar(parent, parent[node]);
     }
 };
+
+// Disjoint Set
+class DisjointSet {
+private:
+    vector<int> parent, rank;
+
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        for (int i = 0; i < n + 1; i ++) {
+            parent[i] = i;
+        }
+    }
+
+    int findPar(int u) {
+        if (u == parent[u]) {
+            return u;
+        }
+
+        return parent[u] = findPar(parent[u]);
+    }
+
+    void unionByRank(int u, int v) {
+        int pu = findPar(u);
+        int pv = findPar(v);
+
+        if (pu == pv) return;
+
+        if (rank[pu] < rank[pv]) {
+            parent[pu] = pv;
+        } else if (rank[pu] > rank[pv]) {
+            parent[pv] = pu;
+        } else {
+            parent[pv] = pu;
+            rank[pu] ++;
+        }
+    }
+};
+
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        
+        int maxRow = 0;
+        int maxCol = 0;
+        for (const vector<int>& stone : stones) {
+            maxRow = max(maxRow, stone[0]);
+            maxCol = max(maxCol, stone[1]);
+        }
+
+        DisjointSet ds(maxRow + maxCol + 1);
+        unordered_set<int> st;
+        for (const vector<int>& stone : stones) {
+            int row = stone[0];
+            int col = stone[1] + maxRow + 1;
+
+            ds.unionByRank(row, col);
+            st.insert(row);
+            st.insert(col);
+        }
+
+        int count = 0;
+        for (const int& x : st) {
+            if (ds.findPar(x) == x) {
+                count ++;
+            }
+        }
+
+        return n - count;
+    }
+};
