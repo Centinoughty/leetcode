fb0ecbe750b5688248f4f2b66fcf77c24751eda1
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-06-17 13:21:34 +0530

    added solution 120

--- 120.cpp
+++ 120.cpp
@@ -31,3 +31,29 @@ private:
         return memo[lvl][pos] = triangle[lvl][pos] + min(left, right);
     }
 };
+
+// Tabulation
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<vector<int>> dp(n, vector<int>());
+        for (int i = 0; i < n; i ++) {
+            dp[i].resize(i + 1, INT_MAX);
+        }
+
+        dp[0][0] = triangle[0][0];
+        int minVal = dp[0][0];
+        for (int i = 1; i < n; i ++) {
+            minVal = INT_MAX;
+            for (int j = 0; j < triangle[i].size(); j ++) {
+                int tLeft = j > 0 ? dp[i - 1][j - 1] : INT_MAX;
+                int tRight = j < i ? dp[i - 1][j] : INT_MAX;
+                dp[i][j] = triangle[i][j] + min(tLeft, tRight);
+                minVal = min(dp[i][j], minVal);
+            }
+        }
+
+        return minVal;
+    }
+};
