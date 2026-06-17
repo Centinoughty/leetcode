7e95c9dcadea46b17fad56eacd9d1425790e0825
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-06-17 12:30:33 +0530

    added solution 64

--- 64.cpp
+++ 64.cpp
@@ -35,3 +35,27 @@ private:
         return grid[x][y];
     }
 };
+
+// Tabulation
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        for (int i = 0; i < m; i ++) {
+            for (int j = 0; j < n; j ++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = grid[i][j];
+                    continue;
+                }
+
+                int top = i > 0 ? dp[i - 1][j] : INT_MAX;
+                int left = j > 0 ? dp[i][j - 1] : INT_MAX;
+                dp[i][j] = grid[i][j] + min(top, left);
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+};
