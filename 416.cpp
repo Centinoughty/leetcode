103411ad90e048c66cd08b18c8755ceca919c8db
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-06-18 10:54:13 +0530

    added solution 416

--- 416.cpp
+++ 416.cpp
@@ -33,3 +33,37 @@ public:
     return false;
   }
 };
+
+// Memoisation
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+        if (totalSum % 2) return false;
+
+        int n = nums.size();
+        memo.resize(n, vector<int>(totalSum / 2 + 1, -1));
+
+        return sumHelper(nums, 0, totalSum / 2);
+    }
+
+private:
+    vector<vector<int>> memo;
+
+    bool sumHelper(vector<int>& nums, int pos, int k) {
+        if (pos == nums.size()) {
+            if (k == 0) return true;
+            return false;
+        }
+
+        if (memo[pos][k] != -1) return memo[pos][k];
+
+        bool noPick = sumHelper(nums, pos + 1, k);
+        bool pick = false;
+        if (k - nums[pos] >= 0) {
+            pick = sumHelper(nums, pos + 1, k - nums[pos]);
+        }
+
+        return memo[pos][k] = pick || noPick;
+    }
+};
