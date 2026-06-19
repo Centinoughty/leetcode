d62f2099c9011d135951b198a491a1e4b1cb3c4f
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-06-19 11:38:58 +0530

    added 1547

---
+++ 1547.cpp
@@ -0,0 +1,41 @@
+// Minimum Cost to Cut a Stick
+// HARD
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minCost(int n, vector<int>& cuts) {
+        sort(cuts.begin(), cuts.end());
+
+        newCuts.push_back(0);
+        for (const int& cut : cuts) {
+            newCuts.push_back(cut);
+        }
+
+        newCuts.push_back(n);
+        int m = newCuts.size();
+        memo.resize(m + 1, vector<int>(m + 1, -1));
+
+        return costHelper(0, m - 1);
+    }
+
+private:
+    vector<int> newCuts;
+    vector<vector<int>> memo;
+
+    int costHelper(int l, int r) {
+        if (memo[l][r] != -1) return memo[l][r];
+
+        if (r - l == 1) return 0;
+
+        int res = INT_MAX;
+        for (int mid = l + 1; mid < r; mid ++) {
+            int cost = costHelper(l, mid) + costHelper(mid, r) + newCuts[r] - newCuts[l];
+            res = min(res, cost);
+        }
+
+        return memo[l][r] = res;
+    }
+};
