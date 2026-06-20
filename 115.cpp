b5f534e3b13293b85ca697e17ffc3e79ee62d06a
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-06-20 11:06:40 +0530

    added 115

---
+++ 115.cpp
@@ -0,0 +1,33 @@
+// Distinct Subsequences
+// HARD
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int l1 = s.length(), l2 = t.length();
+        memo.resize(l1, vector<int>(l2, -1));;
+        return countHelper(s, t, 0, 0);
+    }
+
+private:
+    vector<vector<int>> memo;
+
+    int countHelper(string& s, string& t, int i, int j) {
+        int l1 = s.length(), l2 = t.length();
+        if (i == l1) return (j == l2);
+        if (j == l2) return 1;
+
+        if (memo[i][j] != -1) return memo[i][j];
+    
+        int res = 0;
+        if (s[i] == t[j]) {
+            res += countHelper(s, t, i + 1, j + 1);
+        }
+
+        res += countHelper(s, t, i + 1, j);
+        return memo[i][j] = res;
+    }
+};
