0399bceedf6e265f7f63f2def8c4c706156c4a6a
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-06-20 21:49:05 +0530

    added 3963

---
+++ 3963.cpp
@@ -0,0 +1,21 @@
+// Create Grid with Exactly One Path
+// EASY
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> createGrid(int m, int n) {
+        vector<string> res(m, string(n, '#'));
+        for (int j = 0; j < n; j ++) {
+            res[0][j] = '.';
+        }
+
+        for (int i = 0; i < m; i ++) {
+            res[i][n - 1] = '.';
+        }
+
+        return res;
+    }
+};
