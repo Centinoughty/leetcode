4b397c741707ae674be86373f6c0b7874c81083d
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-06-20 09:42:32 +0530

    added 516

---
+++ 516.cpp
@@ -0,0 +1,24 @@
+// Longest Palindromic Subsequence
+// MEDIUM
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        int n = s.length();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        for (int i = n - 1; i >= 0; i --) {
+            for (int j = 0; j < n; j ++) {
+                if (s[i] == s[j]) {
+                    dp[i][j + 1] = 1 + dp[i + 1][j];
+                } else {
+                    dp[i][j + 1] = max(dp[i + 1][j + 1], dp[i][j]);
+                }
+            }
+        }
+
+        return dp[0][n];
+    }
+};
