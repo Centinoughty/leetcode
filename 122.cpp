58f47320283946aa65bd47ad7c0a3a10bf14985b
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-06-22 12:43:41 +0530

    added solution 122

--- 122.cpp
+++ 122.cpp
@@ -25,3 +25,31 @@ public:
         return dp[0];
     }
 };
+
+// Memoisation
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        memo.resize(prices.size(), vector<int>(2, -1));
+        return profitHelper(prices, 0, 1);
+    }
+
+private:
+    vector<vector<int>> memo;
+
+    int profitHelper(vector<int>& prices, int pos, int buy) {
+        if (pos == prices.size()) return 0;
+
+        if (memo[pos][buy] != -1) return memo[pos][buy];
+
+        int profit = 0;
+        if (buy) {
+            profit = max(-prices[pos] + profitHelper(prices, pos + 1, 0), profitHelper(prices, pos + 1, 1));
+        } else {
+            profit = max(prices[pos] + profitHelper(prices, pos, 1), profitHelper(prices, pos + 1, 0));
+        }
+
+        return memo[pos][buy] = profit;
+    }
+};
+
