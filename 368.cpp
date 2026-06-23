23839ec2cb8688db9e9691d4b64df303d2a29f78
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-06-23 21:47:49 +0530

    added solution 368

--- 368.cpp
+++ 368.cpp
@@ -35,3 +35,38 @@ public:
     return res;
   }
 };
+
+// Tabulation
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+        vector<int> dp(n, 1), hash(n);
+        int idx = 0;
+        for (int i = 0; i < n; i ++) {
+            hash[i] = i;
+            for (int j = 0; j < i; j ++) {
+                if ((nums[j] % nums[i] == 0 || nums[i] % nums[j] == 0) && dp[i] < 1 + dp[j]) {
+                    dp[i] = 1 + dp[j];
+                    hash[i] = j;
+                }
+            }
+
+            if (dp[idx] < dp[i]) {
+                idx = i;
+            }
+        }
+
+        vector<int> res;
+        res.push_back(nums[idx]);
+        while (hash[idx] != idx) {
+            idx = hash[idx];
+            res.push_back(nums[idx]);
+        }
+
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
