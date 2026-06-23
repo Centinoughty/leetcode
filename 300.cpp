ec0977427453521d8989fb3a2e48acec19096059
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-06-23 07:52:27 +0530

    added solution 300

--- 300.cpp
+++ 300.cpp
@@ -30,3 +30,25 @@ private:
         return dp[pos][prev + 1];
     }
 };
+
+// Tabulation
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        for (int i = n - 1; i >= 0; i --) {
+            for (int prev = i - 1; prev >= -1; prev --) {
+                int noPick = dp[i + 1][prev + 1];
+                int pick = 0;
+                if (prev == -1 || nums[i] > nums[prev]) {
+                    pick = 1 + dp[i + 1][i + 1];
+                }
+
+                dp[i][prev + 1] = max(pick, noPick);
+            }
+        }
+
+        return dp[0][0];
+    }
+};
