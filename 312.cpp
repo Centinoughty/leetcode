638e8f752e671805188876a1f2071678c0a19198
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-06-24 20:41:37 +0530

    added 312

---
+++ 312.cpp
@@ -0,0 +1,40 @@
+// Burst Balloons
+// HARD
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        vector<int> temp;
+        temp.push_back(1);
+        for (const int& num : nums) {
+            temp.push_back(num);
+        }
+
+        temp.push_back(1);
+        int n = nums.size();
+        memo.resize(n + 1, vector<int>(n + 1, -1));
+
+        return costHelper(temp, 1, nums.size());
+    }
+
+private:
+    vector<vector<int>> memo;
+
+    int costHelper(vector<int>& nums, int l, int r) {
+        int n = nums.size();
+        if (r < l) return 0;
+
+        if (memo[l][r] != -1) return memo[l][r];
+
+        int res = INT_MIN;
+        for (int k = l; k <= r; k ++) {
+            int cost = nums[l - 1] * nums[k] * nums[r + 1];
+            res = max(res, cost + costHelper(nums, l, k - 1) + costHelper(nums, k + 1, r));
+        }
+
+        return memo[l][r] = res;
+    }
+};
