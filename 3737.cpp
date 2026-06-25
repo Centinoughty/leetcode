c7ed5213ce3c5d72d73763227ff918072d526ca6
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-06-25 07:09:04 +0530

    added 3737

---
+++ 3737.cpp
@@ -0,0 +1,23 @@
+// Count Subarrays with Majority Element I
+// MEDIUM
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countMajoritySubarrays(vector<int>& nums, int target) {
+        int res = 0;
+        for (int i = 0; i < nums.size(); i ++) {
+            int cnt = 0;
+            for (int j = i; j < nums.size(); j ++) {
+                cnt += (nums[j] == target ? 1 : -1);
+                if (cnt > 0) {
+                    res ++;
+                }
+            }
+        }
+
+        return res;
+    }
+};
