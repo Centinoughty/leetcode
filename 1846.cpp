780ce6f3a24d3422382685aebefb1bb6d98bb270
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-06-28 07:30:59 +0530

    added 1846

---
+++ 1846.cpp
@@ -0,0 +1,43 @@
+// Maximum Element After Decreasing and Rearranging
+// MEDIUM
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        int n = arr.size();        
+        unordered_map<int, int> mp;
+        for (const int& num : arr) {
+            mp[num] ++;
+        }
+
+        sort(arr.begin(), arr.end());
+
+        int k = n - 1, num = 1;
+        for (int i = 0; i < n; i ++) {
+            if (i == 0 && arr[i] != 1) {
+                mp[arr[k]] --;
+                if (mp[arr[k]] == 0) mp.erase(arr[k]);
+
+                k --;
+                continue;
+            }
+
+            if (arr[i] == num + 1) {
+                num ++;
+            } else if (arr[i] == num) {
+                continue;
+            } else {
+                num ++;
+                mp[arr[k]] --;
+                if (mp[arr[k]] == 0) mp.erase(arr[k]);
+
+                k --;
+            }
+        }
+
+        return num;
+    }
+};
