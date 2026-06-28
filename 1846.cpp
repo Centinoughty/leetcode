a86590d5c430f05c6ce6c5fb066e705a6f0a4295
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-06-28 07:46:29 +0530

    added solution 1846

--- 1846.cpp
+++ 1846.cpp
@@ -41,3 +41,19 @@ public:
         return num;
     }
 };
+
+// Easy Approach
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int num = 1;
+        for (int i = 1; i < arr.size(); i ++) {
+            if (arr[i] >= num + 1) {
+                num ++;
+            }
+        }
+
+        return num;
+    }
+};
