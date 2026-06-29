13d0e713a47f6100b3f952f0057fa682df8bddfb
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-06-29 09:10:25 +0530

    added solution 1967

--- 1967.cpp
+++ 1967.cpp
@@ -24,3 +24,48 @@ public:
         return count;
     }
 };
+
+// KMP Algorithm
+class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        int count = 0;
+        for (string& pattern : patterns) {
+            if (match(word, pattern)) count ++;
+        }
+
+        return count;
+    }
+
+private:
+    bool match(string& s, string& p) {
+        vector<int> pi = generatePi(p);
+        int i = 0, j = 0;
+        while (i < s.length()) {
+            while (j >= 0 && s[i] != p[j]) j = pi[j];
+
+            i ++;
+            j ++;
+            if (j == p.length()) return true;
+        }
+
+        return false;
+    }
+
+    vector<int> generatePi(string& p) {
+        int n = p.length();
+        vector<int> pi(n + 1, 0);
+        pi[0] = -1;
+
+        int i = 0, j = -1;
+        while (i < n) {
+            while (j >= 0 && p[i] != p[j]) j = pi[j];
+
+            i ++;
+            j ++;
+            pi[i] = j;
+        }
+
+        return pi;
+    }
+};
