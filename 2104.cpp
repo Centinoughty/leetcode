0f0116c98955241bb14477181372aebb24e583c0
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-07-03 12:04:10 +0530

    added solution 2104

--- 2104.cpp
+++ 2104.cpp
@@ -21,3 +21,69 @@ public:
         return res;
     }
 };
+
+// O(n)
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> pse(n, -1), nse(n, n);
+        vector<int> pge(n, -1), nge(n, n);
+
+        stack<int> st;
+        for (int i = 0; i < n; i ++) {
+            while (!st.empty() && nums[st.top()] >= nums[i]) {
+                nse[st.top()] = i;
+                st.pop();
+            }
+
+            st.push(i);
+        }
+
+        while (!st.empty()) st.pop();
+        for (int i = n - 1; i >= 0; i --) {
+            while (!st.empty() && nums[st.top()] > nums[i]) {
+                pse[st.top()] = i;
+                st.pop();
+            }
+
+            st.push(i);
+        }
+
+        while (!st.empty()) st.pop();
+        for (int i = n - 1; i >= 0; i --) {
+            while (!st.empty() && nums[st.top()] < nums[i]) {
+                pge[st.top()] = i;
+                st.pop();
+            }
+
+            st.push(i);
+        }
+
+        while (!st.empty()) st.pop();
+        for (int i = 0; i < n; i ++) {
+            while (!st.empty() && nums[st.top()] <= nums[i]) {
+                nge[st.top()] = i;
+                st.pop();
+            }
+
+            st.push(i);
+        }
+
+        long long small = 0;
+        for (int i = 0; i < n; i ++) {
+            int l = i - pse[i];
+            int r = nse[i] - i;
+            small += 1LL * l * r * nums[i];
+        }
+
+        long long large = 0;
+        for (int i = 0; i < n; i ++) {
+            int l = i - pge[i];
+            int r = nge[i] - i;
+            large += 1LL * l * r * nums[i];
+        }
+
+        return large - small;
+    }
+};
