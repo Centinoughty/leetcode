86958d4aa41fc8ba40dd408b95e294828af242e6
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-07-13 11:47:10 +0530

    added 2220

---
+++ 2220.cpp
@@ -0,0 +1,29 @@
+// Minimum Bit Flips to Convert Number
+// EASY
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int count = 0;
+        while (start && goal) {
+            if ((start & 1) != (goal & 1)) count ++;
+            start >>= 1;
+            goal >>= 1;
+        }
+
+        while (start) {
+            if (start & 1) count ++;
+            start >>= 1;
+        }
+
+        while (goal) {
+            if (goal & 1) count ++;
+            goal >>= 1;
+        }
+
+        return count;
+    }
+};
