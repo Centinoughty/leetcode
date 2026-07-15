2f2566fcd34519ef70db6984b411a74668f9b0c4
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-07-15 13:30:05 +0530

    added 3658

---
+++ 3658.cpp
@@ -0,0 +1,12 @@
+// GCD of Odd and Even Sums
+// EASY
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return gcd(n * n, n * (n + 1));
+    }
+};
