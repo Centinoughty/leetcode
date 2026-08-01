c562b322546304bcedaa39416e6c6a051c4f65b0
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-08-01 09:32:08 +0530

    added solution 486

--- 486.cpp
+++ 486.cpp
@@ -42,3 +42,21 @@ private:
         return memo[l][r] = max(nums[l] - helper(nums, l + 1, r), nums[r] - helper(nums, l, r - 1));
     }
 };
+
+// Tabulation
+class Solution {
+public:
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for (int i = n - 1; i >= 0; i --) {
+            dp[i][i] = nums[i];
+            for (int j = i + 1; j < n; j ++) {
+                dp[i][j] = max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+            }
+        }
+
+        return dp[0][n - 1] >= 0;
+    }
+};
