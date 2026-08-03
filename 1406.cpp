573a114c081fe817536b307f8a90cbcbc07a8f79
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-08-03 08:17:14 +0530

    added 1406

---
+++ 1406.cpp
@@ -0,0 +1,38 @@
+// Stone Game III
+// HARD
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string stoneGameIII(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+        memo.resize(n, INT_MIN);
+        int val = gameHelper(stoneValue, 0);
+
+        if (val > 0) {
+            return "Alice";
+        } else if (val < 0) {
+            return "Bob";
+        } else {
+            return "Tie";
+        }
+    }
+
+private:
+    vector<int> memo;
+
+    int gameHelper(vector<int>& stones, int pos) {
+        int n = stones.size();
+        if (pos == n) return 0;
+
+        if (memo[pos] != INT_MIN) return memo[pos];
+
+        int a = stones[pos] - gameHelper(stones, pos + 1);
+        int b = pos + 1 < n ? stones[pos] + stones[pos + 1] - gameHelper(stones, pos + 2) : INT_MIN;
+        int c = pos + 2 < n ? stones[pos] + stones[pos + 1] + stones[pos + 2] - gameHelper(stones, pos + 3) : INT_MIN;
+
+        return memo[pos] = max(a, max(b, c));
+    }
+};
